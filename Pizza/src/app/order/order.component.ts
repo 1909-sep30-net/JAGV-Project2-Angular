12c18840e8c6b561da98d2272a90952f4330a426
Alvert18 <albertoace_18@outlook.com>
2019-11-10 13:41:33 -0600

    started order page

--- Pizza/src/app/order/order.component.ts
+++ Pizza/src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CreateComponent } from '../create/create.component';
 
 @Component({
   selector: 'app-order',
@@ -6,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+  orderForm: any;
 
-  constructor() { }
+  constructor(public dialog: MatDialog,
+    public dialogRef: MatDialogRef<CreateComponent>,
+    private formBuilder: FormBuilder
+    ) {
+      this.orderForm = this.formBuilder.group({
+        crust: new FormControl('', Validators.required),
+          cheeses: this.formBuilder.array([this.])
+          meats: new FormControl('', Validators.required),
+          vegtables: new FormControl('', Validators.required)
+      });
+    }
 
   ngOnInit() {
   }
